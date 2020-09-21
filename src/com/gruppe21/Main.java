eb20b5377b083002572c44ef99a74a789d40138e
Troels <troelstoy@outlook.com>
2020-09-21 16:43:48 +0200

    Regnemaskine

--- src/com/gruppe21/Main.java
+++ src/com/gruppe21/Main.java
@@ -1,8 +1,30 @@
 package com.gruppe21;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Indtast 2 tal skilt af et mellemrum");
+        Scanner input = new Scanner(System.in);
+
+        float a = input.nextFloat();
+        float b = input.nextFloat();
+
+        System.out.println("Indtast operation");
+        Scanner op1 = new Scanner(System.in);
+
+        String op = op1.next();
+
+
+        if (op.equals("+"))
+        {
+            System.out.println("Dit svar er " + (a + b));
+
+
+        }
+        if(op.equals("-"))
+        {
+            System.out.println("Dit svar er " + (a-b));
+        }
     }
 }
